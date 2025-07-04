012c747754d81dc6f332a84b5656c967a533da28
Zenith <zpengli502@gmail.com>
2025-07-04 18:04:25 +0800

    add day3.py +2

--- pythontest/day3.py
+++ pythontest/day3.py
@@ -24,7 +24,7 @@ stocks = {
 stocks2 = {key: value for key, value in stocks.items() if value > 100}
 print(stocks2)
 
-'''
+
 #设计一个函数，传入任意多个参数，对其中int类型或float类型的元素实现更多的甚至是自定义的二元运算
 def calc(init_value,op_func,*args,**kwargs):   #接收一个初始值 init_value 和一个操作函数 op_func  通过 *args 和 **kwargs 接收任意数量的位置参数和关键字参数
     items = list(args) + list(kwargs.values())  #将所有参数合并成一个列表 items，并遍历其中的数值（int 或 float
@@ -41,3 +41,30 @@ def mul(x,y):
     return x *y
 
 print(calc(1,mul,1,2,3,4,5))
+
+
+
+#Lambda函数
+old_nums = [35, 12, 8, 99, 60, 52]
+new_nums = list(map(lambda x:x ** 2, filter(lambda x:x % 2 == 0,old_nums)))
+print(new_nums)
+
+'''
+
+import random
+import time
+
+def download(filename):
+    #下载文件
+    print(f'开始下载{filename}.')
+    time.sleep(random.random() * 6)
+    print(f'{filename}下载完成.')
+
+def upload(filename):
+    #上传文件
+    print(f'开始上传{filename}.')
+    time.sleep(random.random() * 8)
+    print(f'{filename}上传完成.')
+
+download('MySQL从删库到跑路.avi')
+upload('Python从入门到住院.pdf')
