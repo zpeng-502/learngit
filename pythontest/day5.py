152380e395eb40b80f7d17ef01e19df1daaff75c
Zenith <zpengli502@gmail.com>
2025-07-10 11:04:21 +0800

    add day5.py

---
+++ pythontest/day5.py
@@ -0,0 +1,11 @@
+import csv
+import random
+
+with open('scores.csv', 'w', encoding='utf-8') as file:
+    writer = csv.writer(file, delimiter='|', quoting=csv.QUOTE_ALL)
+    writer.writerow(['姓名', '语文', '数学', '英语'])
+    names = ['关羽', '张飞', '赵云', '马超', '黄忠']
+    for name in names:
+        scores = [random.randrange(50, 101) for _ in range(3)]
+        scores.insert(0, name)
+        writer.writerow(scores)
